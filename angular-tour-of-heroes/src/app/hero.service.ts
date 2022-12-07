ebb9db578e0cabdf3843b353fd30a82d06032bc2
Switch <hk.khandelwal.harsh@gmail.com>
2022-12-08 01:49:43 +0530

    10

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -4,19 +4,37 @@ import { HEROES } from "./mock-heroes";
 import { Observable, of} from "rxjs";
 import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map, tap, catchError } from "rxjs/operators"
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';
+
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+
+    }
+    
+  }
+
   constructor(private messageService: MessageService,
               private http: HttpClient) {
   }
 getHeroes(): Observable<Hero[]>{
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    // const heroes = of(HEROES);
+    // // this.messageService.add('HeroService: fetched heroes');
+    // return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes',[]))
+      );
 }
 getHero(id: number): Observable<Hero>{
     const hero = HEROES.find(h => h.id === id)!;
